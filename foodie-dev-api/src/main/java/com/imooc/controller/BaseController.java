65698a00175999458be90301f76008671155189c
bulletC <codecorner@88.com>
2021-03-28 19:03:44 +0800

    第4周

--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.imooc.controller;
 
 import com.imooc.pojo.Orders;
+import com.imooc.service.center.MyOrdersService;
 import com.imooc.utils.IMOOCJSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,21 +28,21 @@ public class BaseController {
                                                             File.separator + "images" +
                                                             File.separator + "foodie" +
                                                             File.separator + "faces";
-//    public static final String IMAGE_USER_FACE_LOCATION = "/workspaces/images/foodie/faces";
+   // public static final String IMAGE_USER_FACE_LOCATION = "/workspaces/images/foodie/faces";
 
 
-//    @Autowired
-//    public MyOrdersService myOrdersService;
+    @Autowired
+    public MyOrdersService myOrdersService;
 //
-//    /**
-//     * 用于验证用户和订单是否有关联关系，避免非法用户调用
-//     * @return
-//     */
-//    public IMOOCJSONResult checkUserOrder(String userId, String orderId) {
-//        Orders order = myOrdersService.queryMyOrder(userId, orderId);
-//        if (order == null) {
-//            return IMOOCJSONResult.errorMsg("订单不存在！");
-//        }
-//        return IMOOCJSONResult.ok(order);
-//    }
+    /**
+     * 用于验证用户和订单是否有关联关系，避免非法用户调用
+     * @return
+     */
+    public IMOOCJSONResult checkUserOrder(String userId, String orderId) {
+        Orders order = myOrdersService.queryMyOrder(userId, orderId);
+        if (order == null) {
+            return IMOOCJSONResult.errorMsg("订单不存在！");
+        }
+        return IMOOCJSONResult.ok(order);
+    }
 }
